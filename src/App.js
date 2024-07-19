631f326195281f37eafc08007ab2fb2ed4abf4ac
Francisco Zurita <team-rocket@Teams-MacBook-Air.local>
2024-07-19 18:18:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import background from './flowers.jpeg'
+import tarta from './tarta.jpeg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    return (
+  <div class="container" style={{backgroundImage: `url(${background})`}}>
+    <div class="card">
+      <div class="imgBox">
+        <div class="bark"></div>
+        <div class="front">
+          <h1>Feliz cumpleaños!!</h1>
+          <img src={tarta}  width={'200px'} height={'190px'}/>
+          <h3>One last surprise!</h3>
+        </div>
+      </div>
+      <div class="details">
+        <h2 class="color1">Nos congratula decir que has recibido</h2>
+        <h3 class="color2 margin">Un vuelo de ida y vuelta a:</h3>
+        <p>Japón!! </p>
+        <p>Para Enero/Mayo del año que viene:</p>
+        <div class="span"></div>
+        <p>Osaka, Kioto, Hiroshima, Nara,</p>
+        <p>Shirakawago, Takayama, Nagoya, Tokio</p>
+        <div class="span"></div>
+        <p>Espero que lo disfrutes :)</p>
+        <p class="text-right">Happy Birthday</p>
+        <p class="text-right">♥Fran</p>
+      </div>
     </div>
-  );
-}
+  </div>
+    );
+};
 
 export default App;
